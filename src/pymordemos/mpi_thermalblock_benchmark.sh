1f1ada88b0e210fb52d1afa2ff12115d65e975a3
niklasreich <niklas.reich@hs-ruhrwest.de>
2023-12-08 09:45:44 +0100

    adpated/new shell scripts

---
+++ src/pymordemos/mpi_thermalblock_benchmark.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+mpiexec -n 2 python -m thermalblock_script 3 2 9 0
+mpiexec -n 3 python -m thermalblock_script 3 2 9 0
+mpiexec -n 4 python -m thermalblock_script 3 2 9 0
+mpiexec -n 5 python -m thermalblock_script 3 2 9 0
+mpiexec -n 6 python -m thermalblock_script 3 2 9 0
+mpiexec -n 7 python -m thermalblock_script 3 2 9 0
+mpiexec -n 8 python -m thermalblock_script 3 2 9 0
+mpiexec -n 10 python -m thermalblock_script 3 2 9 0
+mpiexec -n 12 python -m thermalblock_script 3 2 9 0
+mpiexec -n 14 python -m thermalblock_script 3 2 9 0
+mpiexec -n 16 python -m thermalblock_script 3 2 9 0
+python -m thermalblock_script 3 2 9 1
+python -m thermalblock_script 3 2 9 2
+python -m thermalblock_script 3 2 9 3
+python -m thermalblock_script 3 2 9 4
+python -m thermalblock_script 3 2 9 5
+python -m thermalblock_script 3 2 9 6
+python -m thermalblock_script 3 2 9 7
+python -m thermalblock_script 3 2 9 8
+python -m thermalblock_script 3 2 9 10
+python -m thermalblock_script 3 2 9 12
+python -m thermalblock_script 3 2 9 14
+python -m thermalblock_script 3 2 9 16
